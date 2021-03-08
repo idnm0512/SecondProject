d1f946a70b8cb257481a986a1b7180e06760ae39
idnm0512 <idnm0512@gmail.com>
2021-03-08 16:13:48 +0900

    added gimoggi

--- Hello.java
+++ Hello.java
@@ -4,5 +4,6 @@ public class Hello {
 		System.out.println("what is git?");
 		System.out.println("That's DVCS");
 		System.out.println("Linux kernel used Bitkeeper DVCS");
+		System.out.println("Ang Gimoggi");
 	}	
 }	
